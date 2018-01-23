e23ff13733d14a1bac4bee824f3ba26c46dc1205
Brennan Heisler <learn@heislercreative.com>
2018-01-23 13:38:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-katz_deli = []
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each_with_index do |customer, i|
+      current_line << " #{i}. #{customer}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(deli, name)
+  deli.push(name)
+  "Welcome, #{name}. You are number #{deli.length} in line."
+end
