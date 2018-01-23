31babc94dc32707489750113109327d4cce4826a
Brennan Heisler <learn@heislercreative.com>
2018-01-23 13:41:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,9 @@ end
 
 def take_a_number(deli, name)
   deli.push(name)
-  "Welcome, #{name}. You are number #{deli.length} in line."
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+
 end
